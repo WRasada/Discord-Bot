2a4b4a621fe6bb45233f28fefdcffc2abb70bfb8
Wesley Rasada <wesleyrasada@gmail.com>
2021-09-06 18:24:06 -0700

    Updated embed message

--- casino2/games.py
+++ casino2/games.py
@@ -314,7 +314,6 @@ class Blackjack:
     @staticmethod
     def bj_embed(ctx, ph, dh, count1, initial=False, outcome=None):
         hand = _("{}\n**Score:** {}")
-        empty = _(" ")
         footer = _("Cards in Deck: {}")
         start = _(":regional_indicator_h: = Hit\n:regional_indicator_s: = Stay\n:regional_indicator_d: = Double")
         after = _("**Options:** hit or stay")
@@ -328,7 +327,6 @@ class Blackjack:
             name=_("{}'s Hand").format(ctx.author.name),
             value=hand.format(", ".join(deck.fmt_hand(ph)), count1),
         )
-        embed.add_field(name="\u200b", value=empty, inline=False)
         embed.add_field(
             name=_("{}'s Hand").format(ctx.bot.user.name), value=hand.format(dealer_hand, count2)
         )
